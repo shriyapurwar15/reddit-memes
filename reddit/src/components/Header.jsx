c370e01237b87e61c47d683512c7cddc544483c1
shriyapurwar15 <shriyapurwar@gmail.com>
2023-05-24 22:42:17 +0530

    reddit

---
+++ reddit/src/components/Header.jsx
@@ -0,0 +1,36 @@
+
+// import {AppBar,Toolbar} from '@mui/material';
+
+
+
+// const Header = () => {
+//     const imageurl="https://1000logos.net/wp-content/uploads/2017/05/Reddit-logo.jpg";
+//   return (
+//     <AppBar color="action" position="static">
+//         <Toolbar>
+//           <img src={imageurl} alt="imageurl" style={{width:100}}/>
+//         </Toolbar>
+//     </AppBar>
+//   )
+// }
+
+// export default Header;
+
+
+
+import { AppBar, Toolbar } from '@mui/material';
+
+const Header = () => {
+
+    const logo = 'https://1000logos.net/wp-content/uploads/2017/05/Reddit-logo.jpg';
+
+    return (
+        <AppBar color="action" position='static'>
+            <Toolbar>
+                <img src={logo} alt="logo" style={{ width: 100 }} />
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
